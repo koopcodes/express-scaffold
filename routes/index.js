7422ffca76857e7fbe1e6cb2e80429e8fc3b8124
koopdev <koop@koop.codes>
2018-11-07 16:39:43 -0800

    Server and Favicon Only

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var port = 3000;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  console.log('port: ', port);
+  res.render('index', {
+    title: 'Express',
+    port: port,
+  });
 });
 
 module.exports = router;
